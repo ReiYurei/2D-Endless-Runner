5cb32af9c380c27a5236c06c3b656358d3ae5d4f
ReiYurei <106614665+ReiYurei@users.noreply.github.com>
2022-06-27 22:01:37 +0700

    Using area for auto generate terrain
    - Add terrain script controller to template prefab
    - add terrain generator script to container

--- Assets/Script/TerrainTemplateController.cs
+++ Assets/Script/TerrainTemplateController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class TerrainTemplateController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Templates")]
+    public List<TerrainTemplateController> terrainTemplates;
+    public float terrainTemplateWidth;
+    [Header("Generator Area")]
+    public Camera gameCamera;
+    public float areaStartOffset;
+    public float areaEndOffset;
+    private const float debugLineHeight = 10.0f;
+    private float GetHorizontalPositionStart()
     {
-        
+        return gameCamera.ViewportToWorldPoint(new Vector2(0f, 0f)).x +
+        areaStartOffset;
     }
-
-    // Update is called once per frame
-    void Update()
+    private float GetHorizontalPositionEnd()
     {
-        
+        return gameCamera.ViewportToWorldPoint(new Vector2(1f, 0f)).x + areaEndOffset;
+    }
+    // debug
+    private void OnDrawGizmos()
+    {
+        Vector3 areaStartPosition = transform.position;
+        Vector3 areaEndPosition = transform.position;
+        areaStartPosition.x = GetHorizontalPositionStart();
+        areaEndPosition.x = GetHorizontalPositionEnd();
+        Debug.DrawLine(areaStartPosition + Vector3.up * debugLineHeight / 2,
+        areaStartPosition + Vector3.down * debugLineHeight / 2, Color.red);
+        Debug.DrawLine(areaEndPosition + Vector3.up * debugLineHeight / 2,
+        areaEndPosition + Vector3.down * debugLineHeight / 2, Color.red);
     }
 }
