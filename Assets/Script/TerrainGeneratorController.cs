ef0efa21ff842af1f2d24acbc387a72fb415b6b0
ReiYurei <106614665+ReiYurei@users.noreply.github.com>
2022-07-15 18:04:44 +0700

    Terrain Object Pooling
    - add and initialise variable pool Dictionary
    - add function GenerateFromPool and
    ReturnToPool
    - change Instantiate statement with
    GenerateFromPool
    - change Destroy statement with ReturnToPool

--- Assets/Script/TerrainGeneratorController.cs
+++ Assets/Script/TerrainGeneratorController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TerrainGeneratorController : MonoBehaviour
 {
+    private Dictionary<string, List<GameObject>> pool;
+
     [Header("Templates")]
     public List<TerrainTemplateController> terrainTemplates;
     public float terrainTemplateWidth;
@@ -20,10 +22,15 @@ public class TerrainGeneratorController : MonoBehaviour
     
     private float lastRemovedPositionX;
     private float lastGeneratedPositionX;
-   void Start()
+    void Start()
     {
+        // init pool
+        pool = new Dictionary<string, List<GameObject>>();
+
         spawnedTerrain = new List<GameObject>();
+
         lastGeneratedPositionX = GetHorizontalPositionStart();
+        lastRemovedPositionX = lastGeneratedPositionX - terrainTemplateWidth;
         foreach (TerrainTemplateController terrain in earlyTerrainTemplates)
         {
             GenerateTerrain(lastGeneratedPositionX, terrain);
@@ -32,7 +39,7 @@ public class TerrainGeneratorController : MonoBehaviour
         while (lastGeneratedPositionX < GetHorizontalPositionEnd())
         {
             GenerateTerrain(lastGeneratedPositionX);
-        lastGeneratedPositionX += terrainTemplateWidth;
+            lastGeneratedPositionX += terrainTemplateWidth;
         }
     }
     private void GenerateTerrain(float posX, TerrainTemplateController forceterrain =
@@ -49,7 +56,8 @@ null)
         {
             item = forceterrain.gameObject;
         }
-        GameObject newTerrain = Instantiate(item, transform);
+        //GameObject newTerrain = Instantiate(item, transform);
+        GameObject newTerrain = GenerateFromPool(item, transform);
         newTerrain.transform.position = new Vector2(posX, 0f);
         spawnedTerrain.Add(newTerrain);
     }
@@ -60,12 +68,12 @@ null)
             GenerateTerrain(lastGeneratedPositionX);
             lastGeneratedPositionX += terrainTemplateWidth;
         }
-        while (lastRemovedPositionX + terrainTemplateWidth < GetHorizontalPositionStart())
+        while (lastRemovedPositionX + terrainTemplateWidth <
+        GetHorizontalPositionStart())
         {
             lastRemovedPositionX += terrainTemplateWidth;
             RemoveTerrain(lastRemovedPositionX);
         }
-
     }
     private void RemoveTerrain(float posX)
     {
@@ -83,11 +91,46 @@ null)
         if (terrainToRemove != null)
         {
             spawnedTerrain.Remove(terrainToRemove);
-            Destroy(terrainToRemove);
+            //Destroy(terrainToRemove);
+            ReturnToPool(terrainToRemove);
         }
     }
+    private GameObject GenerateFromPool(GameObject item, Transform parent)
+    {
+        if (pool.ContainsKey(item.name))
+        {
+            // if item available in pool
+            if (pool[item.name].Count > 0)
+            {
+                GameObject newItemFromPool = pool[item.name][0];
+                pool[item.name].Remove(newItemFromPool);
+                newItemFromPool.SetActive(true);
+                return newItemFromPool;
+            }
+        }
+        else
+        {
+            // if item list not defined, create new one
+            pool.Add(item.name, new List<GameObject>());
+        }
+        // create new one if no item available in pool
+        GameObject newItem = Instantiate(item, parent);
+        newItem.name = item.name;
+        return newItem;
+    }
+    private void ReturnToPool(GameObject item)
+    {
+        if (!pool.ContainsKey(item.name))
+        {
+            Debug.LogError("INVALID POOL ITEM!!");
+        }
+        pool[item.name].Add(item);
+        item.SetActive(false);
 
-    private float GetHorizontalPositionStart()
+    }
+
+
+        private float GetHorizontalPositionStart()
     {
         return gameCamera.ViewportToWorldPoint(new Vector2(0f, 0f)).x +
         areaStartOffset;
