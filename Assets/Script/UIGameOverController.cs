73c226d9a6def2603feb1d3843d5dbebc02aac86
ReiYurei <106614665+ReiYurei@users.noreply.github.com>
2022-07-15 20:29:44 +0700

    Adding Game Over Behaviour
    - add GameOver UI
    - add GameOver animation
    - add UIGameOverController script
    - add GameOver function

---
+++ Assets/Script/UIGameOverController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class UIGameOverController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // reload
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
